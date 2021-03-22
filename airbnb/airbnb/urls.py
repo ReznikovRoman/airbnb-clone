c4ed2c9157f720d28c8b4de30381d63215a08f54
Roman Reznikov <romanreznikov2002@yandex.ru>
2021-03-22 20:37:41 +0300

    Configure flatpages and add 2 static pages

--- airbnb/airbnb/urls.py
+++ airbnb/airbnb/urls.py
@@ -3,13 +3,20 @@ import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.contrib.flatpages import views as flatpage_views
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pages/', include('django.contrib.flatpages.urls')),
 
     path('', include('main.urls')),
+]
+
+urlpatterns += [
+    path('about/', flatpage_views.flatpage, {'url': 'about/'}, name='about'),
+    path('contacts/', flatpage_views.flatpage, {'url': 'contacts/'}, name='contacts'),
 ]
 
 
