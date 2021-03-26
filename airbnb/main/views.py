5fc110799298345953a75bfbd0dd121e9ec4b42e
Roman Reznikov <romanreznikov2002@yandex.ru>
2021-03-26 21:17:12 +0300

    Add new context variable to the index view

--- airbnb/main/views.py
+++ airbnb/main/views.py
@@ -3,7 +3,11 @@ from django.http import HttpRequest
 
 
 def index(request: HttpRequest):
+    turn_on_block = False
     return render(
         request,
         'index.html',
+        context={
+            'turn_on_block': turn_on_block,
+        }
     )
