5e7b1bf97a1bccecf49a1d7b529562fe439fca7e
Roman Reznikov <romanreznikov2002@yandex.ru>
2021-03-27 18:50:11 +0300

    Add new fields to the Address model (city and country slugs)

--- airbnb/addresses/admin.py
+++ airbnb/addresses/admin.py
@@ -5,10 +5,10 @@ from .models import Address
 
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
-    list_display = ('address_link', 'country', 'city', 'street')
+    list_display = ('address_link', 'country', 'city', 'street', 'city_slug')
     list_filter = ('country',)
+    exclude = ('city_slug', 'country_slug')
 
     def address_link(self, obj: Address):
         return f"Address #{obj.id}"
     address_link.short_description = 'address'
-
