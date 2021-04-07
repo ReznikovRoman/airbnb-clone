2c67108b35b69faba5eec77f1f236bb37a2deacc
Roman Reznikov <romanreznikov2002@yandex.ru>
2021-04-07 23:42:57 +0300

    Fix - set user group only on sign up

--- airbnb/accounts/signals.py
+++ airbnb/accounts/signals.py
@@ -6,6 +6,7 @@ from django.db.models.signals import post_save
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def add_user_to_group(sender, instance, created, **kwargs):
-    common_users_group = Group.objects.get_or_create(name='common_users')[0]
-    instance.groups.add(common_users_group)
-    instance.save()
+    if created:
+        common_users_group = Group.objects.get_or_create(name='common_users')[0]
+        instance.groups.add(common_users_group)
+        instance.save()
