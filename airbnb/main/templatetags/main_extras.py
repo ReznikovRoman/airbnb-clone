edd146f7f2da2baddef2e7202f86c07557175be7
Roman Reznikov <romanreznikov2002@yandex.ru>
2021-04-09 22:43:10 +0300

    Add new template filter, remove redundant ones

--- airbnb/main/templatetags/main_extras.py
+++ airbnb/main/templatetags/main_extras.py
@@ -1,23 +1,8 @@
-from typing import Optional
-import datetime
-
 from django import template
-from django.utils import timezone
+from django.conf import settings
 
 
 register = template.Library()
-
-
-@register.simple_tag(name='current_time')
-def get_current_time(format_string: Optional[str] = None) -> datetime.datetime:
-    if format_string:
-        return timezone.now().strftime(format_string)
-    return timezone.now()
-
-
-@register.filter(name='reverse')
-def reverse_string(string: str) -> str:
-    return string[::-1]
 
 
 @register.simple_tag(takes_context=True)
@@ -27,3 +12,9 @@ def url_replace(context, **kwargs) -> str:
     query_params.pop('page', None)
     query_params.update(kwargs)
     return query_params.urlencode()
+
+
+@register.filter(name='has_group')
+def has_group(user: settings.AUTH_USER_MODEL, group_name: str) -> bool:
+    """Check whether user has the given group or not."""
+    return user.groups.filter(name=group_name).exists()
