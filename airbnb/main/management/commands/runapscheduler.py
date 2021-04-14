0f894229b65d4f89cd212bef3d7557d50299d511
Roman Reznikov <romanreznikov2002@yandex.ru>
2021-04-14 16:55:35 +0300

    Add custom management command to start apscheduler

---
+++ airbnb/main/management/commands/runapscheduler.py
@@ -0,0 +1,47 @@
+from django.conf import settings
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django.core.management.base import BaseCommand
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
+
+from subscribers.services import email_subscribers_about_latest_realty
+
+
+def delete_old_job_executions(max_age=604_800):
+    """This job deletes all apscheduler job executions older than `max_age` from the database."""
+    DjangoJobExecution.objects.delete_old_job_executions(max_age)
+
+
+class Command(BaseCommand):
+    help = "Runs apscheduler."
+
+    def handle(self, *args, **options):
+        scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+        scheduler.add_jobstore(DjangoJobStore(), "default")
+
+        scheduler.add_job(
+            email_subscribers_about_latest_realty,
+            trigger=CronTrigger(
+                day_of_week="fri", hour="18", minute="00"
+            ),  # 18:00 on Friday
+            id="email_newsletter",
+            max_instances=1,
+            replace_existing=True,
+        )
+
+        scheduler.add_job(
+            delete_old_job_executions,
+            trigger=CronTrigger(
+                day_of_week="mon", hour="00", minute="00"
+            ),  # Midnight on Monday, before start of the next work week.
+            id="delete_old_job_executions",
+            max_instances=1,
+            replace_existing=True,
+        )
+
+        try:
+            scheduler.start()
+        except KeyboardInterrupt:
+            scheduler.shutdown()
