6599863a57bacd9f4887cbf21dcb25e4938cf1cd
Roman Reznikov <romanreznikov2002@yandex.ru>
2021-04-22 20:12:06 +0300

    Fix condition in a service

--- airbnb/hosts/services.py
+++ airbnb/hosts/services.py
@@ -6,7 +6,7 @@ from accounts.models import get_default_profile_image_full_url
 def has_user_profile_image(user: settings.AUTH_USER_MODEL) -> bool:
     """Check if the User that wants to become a Host has a profile image."""
     if (
-            user.profile.profile_image.exists() and
+            user.profile.profile_image and
             user.profile.profile_image.url != get_default_profile_image_full_url()
     ):
         return True
