5834f3a75a1885f2ba68bc4e4c5761441aeaade2
Roman Reznikov <romanreznikov2002@yandex.ru>
2021-04-28 17:25:23 +0300

    Add custom admin actions and Model filters

--- airbnb/realty/admin.py
+++ airbnb/realty/admin.py
@@ -1,6 +1,22 @@
+from django.http import HttpRequest
 from django.contrib import admin
+from django.db.models.query import QuerySet
 
 from .models import Amenity, Realty, RealtyImage
+
+
+def make_realty_available(modeladmin: "RealtyAdmin", request: HttpRequest, queryset: QuerySet[Realty]) -> None:
+    queryset.update(is_available=True)
+
+
+make_realty_available.short_description = "Make selected realty available"
+
+
+def make_realty_unavailable(modeladmin: "RealtyAdmin", request: HttpRequest, queryset: QuerySet[Realty]) -> None:
+    queryset.update(is_available=False)
+
+
+make_realty_unavailable.short_description = "Make selected realty unavailable"
 
 
 @admin.register(Amenity)
@@ -16,12 +32,23 @@ class AmenityInline(admin.TabularInline):
 class RealtyAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'name', 'realty_type', 'is_available', 'created', 'host')
     search_fields = ('name',)
+    list_filter = ('location__city', 'is_available',)
+    fieldsets = (
+        ('General', {
+            'fields': ('name', 'slug', 'location', 'host'),
+        }),
+        ('Realty info', {
+            'fields': ('description', 'is_available',
+                       'realty_type', 'beds_count', 'max_guests_count', 'price_per_night')
+        }),
+    )
     inlines = [
         AmenityInline,
     ]
     prepopulated_fields = {
         'slug': ('name',)
     }
+    actions = [make_realty_available, make_realty_unavailable]
 
 
 @admin.register(RealtyImage)
