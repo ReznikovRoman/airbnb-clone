7e7f97444d95e0ab81fd8e5aa5af839c6104946d
Roman Reznikov <romanreznikov2002@yandex.ru>
2021-05-16 17:10:02 +0300

    Reformat 'hosts/models' tests

--- airbnb/hosts/tests/test_models.py
+++ airbnb/hosts/tests/test_models.py
@@ -1,6 +1,7 @@
 from model_bakery import baker
 
 from django.test import TestCase
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 from ..models import RealtyHost
 
@@ -15,17 +16,28 @@ class RealtyHostModelTests(TestCase):
             host_rating=4,
         )
 
-    def test_host_rating_verbose_name(self):
-        """Test that host_rating verbose name is set correctly."""
-        host: RealtyHost = RealtyHost.objects.first()
-        host_rating_verbose_name = host._meta.get_field('host_rating').verbose_name
-        self.assertEqual(host_rating_verbose_name, 'rating')
+    def test_model_verbose_name_single(self):
+        """Test that model verbose name is set correctly."""
+        self.assertEqual(RealtyHost._meta.verbose_name, 'realty host')
+
+    def test_model_verbose_name_plural(self):
+        """Test that model verbose name (in plural) is set correctly."""
+        self.assertEqual(RealtyHost._meta.verbose_name_plural, 'realty hosts')
+
+    def test_host_rating_field_params(self):
+        """Test that host_rating field has all required parameters."""
+        host_rating_field = RealtyHost._meta.get_field('host_rating')
+
+        self.assertEqual(host_rating_field.verbose_name, 'rating')
+        self.assertTrue(host_rating_field.blank)
+        self.assertTrue(host_rating_field.null)
+        self.assertIn(MinValueValidator(0), host_rating_field.validators)
+        self.assertIn(MaxValueValidator(5), host_rating_field.validators)
 
     def test_object_name_has_first_name_and_last_name(self):
-        """Test that object name is set up properly."""
+        """Test that RealtyHost object name is set up properly."""
         host: RealtyHost = RealtyHost.objects.first()
-        expected_object_name = f"Host: {host.user.first_name} {host.user.last_name}"
-        self.assertEqual(str(host), expected_object_name)
+        self.assertEqual(str(host), f"Host: {host.user.first_name} {host.user.last_name}")
 
     def test_host_has_appropriate_group(self):
         """Test that the `host.user` is in the right group (`hosts`)."""
