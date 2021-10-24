6a13dbc5783deebbf2495d47f82045af39d8fa7d
Roman Reznikov <romanreznikov2002@yandex.ru>
2021-10-24 17:52:34 +0300

    #58 feat(prod settings): configure ALLOWED_HOSTS in production settings

--- airbnb_app/airbnb/settings/pro.py
+++ airbnb_app/airbnb/settings/pro.py
@@ -4,10 +4,9 @@ from .base import *  # noqa: F401, F403
 DEBUG = False
 
 ADMINS = (
-    ('Roman R', 'esl.manager.mail@gmail.com'),
+    ('Roman R', os.environ.get('PROJECT_ADMIN_EMAIL', 'esl.manager.mail@gmail.com')),  # noqa: F405
 )
 
+ALLOWED_HOSTS = os.environ.get('PROJECT_ALLOWED_HOSTS', '').split(',')  # noqa: F405
 
-# TODO: configure allowed hosts and ensure secure connection
-# HOSTS
-ALLOWED_HOSTS = ['*']
+# TODO: ensure secure connection
