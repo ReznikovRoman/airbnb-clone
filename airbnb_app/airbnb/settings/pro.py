15f1fd3b605114f32e8f21702517f516e2e7dbb1
Roman Reznikov <romanreznikov2002@yandex.ru>
2021-10-28 17:23:11 +0300

    #73 fix(prod settings): fix production DB settings

--- airbnb_app/airbnb/settings/pro.py
+++ airbnb_app/airbnb/settings/pro.py
@@ -5,4 +5,21 @@ DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get('PROJECT_ALLOWED_HOSTS', '').split(',')  # noqa: F405
 
-# TODO: ensure secure connection
+# DATABASE
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # noqa: F405
+        'NAME': os.environ.get('POSTGRES_PROD_DB', 'airbnb_thinknetica'),  # noqa: F405
+        'USER': os.environ.get('POSTGRES_PROD_USER', 'postgres'),  # noqa: F405
+        'PASSWORD': os.environ.get('POSTGRES_PROD_PASSWORD', 'postgres'),  # noqa: F405
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),  # noqa: F405
+        'PORT': os.environ.get('POSTGRES_PORT', 5432),  # noqa: F405
+    },
+}
+
+# HTTPS
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_NAME = 'session_airbnb'
+SESSION_COOKIE_AGE = 7776000
+SESSION_COOKIE_SECURE = True
