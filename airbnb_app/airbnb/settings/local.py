9bd6cd32065803e42f7733b44d03a1ca2c1dc064
Roman Reznikov <romanreznikov2002@yandex.ru>
2021-10-28 17:58:08 +0300

    #73 fix(db settings): fix db settings

--- airbnb_app/airbnb/settings/local.py
+++ airbnb_app/airbnb/settings/local.py
@@ -8,6 +8,19 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
+# DATABASE
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # noqa: F405
+        'NAME': os.environ.get('POSTGRES_DB', 'airbnb_thinknetica'),  # noqa: F405
+        'USER': os.environ.get('POSTGRES_DEFAULT_USER', 'postgres'),  # noqa: F405
+        'PASSWORD': os.environ.get('POSTGRES_DEFAULT_PASSWORD', 'postgres'),  # noqa: F405
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),  # noqa: F405
+        'PORT': os.environ.get('POSTGRES_PORT', 5432),  # noqa: F405
+    },
+}
+
+
 # DEBUG TOOLBAR
 INTERNAL_IPS = [
     '127.0.0.1',
