55443f0f5088a36c9104798cb24808f47d0997d8
Roman Reznikov <romanreznikov2002@yandex.ru>
2021-11-01 16:33:24 +0300

    #63 chore(local settings): refactor local settings Enable django-debug-toolbar; fix daphne local configuration; remove redundant containers from local docker-compose

--- airbnb_app/airbnb/settings/local.py
+++ airbnb_app/airbnb/settings/local.py
@@ -26,10 +26,13 @@ MEDIA_ROOT = BASE_DIR / 'airbnb/media/'
 
 
 # DEBUG TOOLBAR
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": lambda show_toolbar: True,
+}
 INTERNAL_IPS = [
     '127.0.0.1',
-    '172.18.0.1',
     'localhost',
+    'server',
 ]
 mimetypes.add_type('application/javascript', '.js', True)
 
