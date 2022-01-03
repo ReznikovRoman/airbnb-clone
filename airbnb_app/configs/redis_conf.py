688dc0290f31ccb8ae83d0eeb7bf6cb9b752fcdf
Roman Reznikov <romanreznikov2002@yandex.ru>
2022-01-03 14:51:55 +0300

    feat(redis): add basic redis sentinel configuration

--- airbnb_app/configs/redis_conf.py
+++ airbnb_app/configs/redis_conf.py
@@ -1,4 +1,5 @@
 import redis
+from redis.sentinel import Sentinel
 
 from django.conf import settings
 
@@ -10,3 +11,16 @@ redis_instance = redis.Redis(
     charset=settings.DEFAULT_CHARSET,
     decode_responses=settings.REDIS_DECODE_RESPONSES,
 )
+if settings.PROJECT_ENVIRONMENT == settings.EnvironmentType.PROD.value:
+    hosts = settings.REDIS_SENTINEL_HOSTS
+    sentinel = Sentinel(
+        sentinels=[(host, 26379) for host in hosts],
+        socket_timeout=0.1,
+        ssl=True,
+        ssl_ca_certs=settings.REDIS_SSL_CERT_DOCKER_PATH,
+    )
+    redis_instance: redis.Redis = sentinel.master_for(
+        service_name=settings.REDIS_CLUSTER_NAME,
+        password=settings.REDIS_CLUSTER_PASSWORD,
+        decode_responses=settings.REDIS_DECODE_RESPONSES,
+    )
