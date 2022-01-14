201c47e162b3aa9f94246c71306c14784ab8ffaf
Roman Reznikov <romanreznikov2002@yandex.ru>
2022-01-14 14:49:26 +0300

    #134: refactor: changec Cloud Functions files' structure

--- yandex/cloud_functions/delete_image/deploy.sh
+++ yandex/cloud_functions/delete_image/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source ./src/.env
+source ./.env
 
 function create_cloud_function {
     yc serverless function create \
@@ -9,17 +9,18 @@ function create_cloud_function {
 }
 
 function create_cloud_function_version {
-  yc serverless function version create \
-    --function-name="$YANDEX_FUNCTION_DELETE_IMAGE_NAME" \
-    --service-account-id="$YANDEX_FUNCTION_DELETE_IMAGE_SERVICE_ACCOUNT_ID" \
-    --cloud-id="$YANDEX_FUNCTION_DELETE_IMAGE_CLOUD_ID" \
-    --folder-id="$YANDEX_FUNCTION_DELETE_IMAGE_FOLDER_ID" \
-    --runtime python39 \
-    --entrypoint main.handler \
-    --memory 128m \
-    --execution-timeout 60s \
-    --source-path ./build.zip \
-    --environment \
+  if [ "$YANDEX_CLOUD_FUNCTIONS_DEBUG" == "1" ]; then
+    yc serverless function version create \
+      --function-name="$YANDEX_FUNCTION_DELETE_IMAGE_NAME" \
+      --service-account-id="$YANDEX_FUNCTION_DELETE_IMAGE_SERVICE_ACCOUNT_ID" \
+      --cloud-id="$YANDEX_FUNCTION_DELETE_IMAGE_CLOUD_ID" \
+      --folder-id="$YANDEX_FUNCTION_DELETE_IMAGE_FOLDER_ID" \
+      --runtime python39 \
+      --entrypoint main.handler \
+      --memory 128m \
+      --execution-timeout 60s \
+      --source-path ./build.zip \
+      --environment \
 YANDEX_CLOUD_FUNCTIONS_AWS_ACCESS_KEY_ID="$YANDEX_CLOUD_FUNCTIONS_AWS_ACCESS_KEY_ID",\
 YANDEX_CLOUD_FUNCTIONS_AWS_ACCESS_KEY_SECRET="$YANDEX_CLOUD_FUNCTIONS_AWS_ACCESS_KEY_SECRET",\
 YANDEX_CLOUD_FUNCTIONS_AWS_DEFAULT_REGION="$YANDEX_CLOUD_FUNCTIONS_AWS_DEFAULT_REGION",\
@@ -29,6 +30,30 @@ YANDEX_FUNCTION_DELETE_IMAGE_NAME="$YANDEX_FUNCTION_DELETE_IMAGE_NAME",\
 YANDEX_FUNCTION_DELETE_IMAGE_BUCKET_NAME="$YANDEX_FUNCTION_DELETE_IMAGE_BUCKET_NAME",\
 YANDEX_FUNCTION_DELETE_IMAGE_OBJECT_STORAGE_MEDIA_PREFIX="$YANDEX_FUNCTION_DELETE_IMAGE_OBJECT_STORAGE_MEDIA_PREFIX",\
 YANDEX_FUNCTION_DELETE_IMAGE_OBJECT_STORAGE_MEDIA_RESIZED_PREFIX="$YANDEX_FUNCTION_DELETE_IMAGE_OBJECT_STORAGE_MEDIA_RESIZED_PREFIX"
+  else
+    yc serverless function version create \
+      --async	\
+      --no-user-output \
+      --function-name="$YANDEX_FUNCTION_DELETE_IMAGE_NAME" \
+      --service-account-id="$YANDEX_FUNCTION_DELETE_IMAGE_SERVICE_ACCOUNT_ID" \
+      --cloud-id="$YANDEX_FUNCTION_DELETE_IMAGE_CLOUD_ID" \
+      --folder-id="$YANDEX_FUNCTION_DELETE_IMAGE_FOLDER_ID" \
+      --runtime python39 \
+      --entrypoint main.handler \
+      --memory 128m \
+      --execution-timeout 60s \
+      --source-path ./build.zip \
+      --environment \
+YANDEX_CLOUD_FUNCTIONS_AWS_ACCESS_KEY_ID="$YANDEX_CLOUD_FUNCTIONS_AWS_ACCESS_KEY_ID",\
+YANDEX_CLOUD_FUNCTIONS_AWS_ACCESS_KEY_SECRET="$YANDEX_CLOUD_FUNCTIONS_AWS_ACCESS_KEY_SECRET",\
+YANDEX_CLOUD_FUNCTIONS_AWS_DEFAULT_REGION="$YANDEX_CLOUD_FUNCTIONS_AWS_DEFAULT_REGION",\
+YANDEX_FUNCTION_DELETE_IMAGE_IAM_TOKEN="$YANDEX_FUNCTION_DELETE_IMAGE_IAM_TOKEN",\
+YANDEX_FUNCTION_DELETE_IMAGE_FOLDER_ID="$YANDEX_FUNCTION_DELETE_IMAGE_FOLDER_ID",\
+YANDEX_FUNCTION_DELETE_IMAGE_NAME="$YANDEX_FUNCTION_DELETE_IMAGE_NAME",\
+YANDEX_FUNCTION_DELETE_IMAGE_BUCKET_NAME="$YANDEX_FUNCTION_DELETE_IMAGE_BUCKET_NAME",\
+YANDEX_FUNCTION_DELETE_IMAGE_OBJECT_STORAGE_MEDIA_PREFIX="$YANDEX_FUNCTION_DELETE_IMAGE_OBJECT_STORAGE_MEDIA_PREFIX",\
+YANDEX_FUNCTION_DELETE_IMAGE_OBJECT_STORAGE_MEDIA_RESIZED_PREFIX="$YANDEX_FUNCTION_DELETE_IMAGE_OBJECT_STORAGE_MEDIA_RESIZED_PREFIX"
+  fi
 
   yc serverless function allow-unauthenticated-invoke \
     --name="$YANDEX_FUNCTION_DELETE_IMAGE_NAME" \
@@ -39,7 +64,7 @@ YANDEX_FUNCTION_DELETE_IMAGE_OBJECT_STORAGE_MEDIA_RESIZED_PREFIX="$YANDEX_FUNCTI
 function deploy {
   make check
   make compile-requirements
-  python3 src/build.py
+  python3 ./build.py
   create_cloud_function || echo "Function '$YANDEX_FUNCTION_DELETE_IMAGE_NAME' is already created."
   create_cloud_function_version
 }
